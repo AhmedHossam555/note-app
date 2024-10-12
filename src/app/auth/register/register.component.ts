1808dfd93709a1f4b0f6d011799c7fa195a35f60
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-10-12 03:23:26 +0300

    update register validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../shared/services/auth.service';
 
@@ -10,6 +10,9 @@ import { AuthService } from '../../shared/services/auth.service';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  isloading:WritableSignal<boolean> = signal(false);
+  errMsg:WritableSignal<string> = signal('');
+  
   private readonly _Auth = inject(AuthService);
   signupForm = new FormGroup({
     name: new FormControl(null, [Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
@@ -19,14 +22,18 @@ export class RegisterComponent {
     phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
   })
   onSubmit(){
+    this.isloading.set(true)
     console.log(this.signupForm.value);
     this._Auth.signup(this.signupForm.value).subscribe({
       next:(res)=>{
         if(res.msg === "done"){
+          this.isloading.set(false)
           window.localStorage.setItem('userToken', res.user.password)
         }
       },
       error:(err)=>{
+        this.isloading.set(false);
+        this.errMsg.set(err.error.msg)
         console.log(err)
       }
     })
