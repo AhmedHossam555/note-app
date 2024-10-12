3a07cd9549e1d9cb419578e4347d60bb99cbaec6
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-10-13 01:08:35 +0300

    add note

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotesService } from '../../shared/services/notes.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  private _NotesService = inject(NotesService)
+  noteForm = new FormGroup({
+    title: new FormControl(null, Validators.required),
+    content: new FormControl(null, Validators.required)
+  })
 
+  onSubmit(){
+    this._NotesService.addNote(this.noteForm.value).subscribe({
+      next: (res)=>{
+        if(res.msg == "done"){
+          
+        }
+      },
+      error: (err)=>{
+        console.log(err)
+      }
+    })
+  }
 }
---
+++ src/app/shared/services/notes.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Enviroment } from '../base/enviroment/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  constructor(private _http:HttpClient) { }
+  addNote(data:object):Observable<any>{
+    return this._http.post(`${Enviroment.baseUrl}/notes`,data,{
+      headers:{
+        token: '3b8ny__' + window.localStorage.getItem('userToken')
+      }
+    })
+  }
+  getUserNotes():Observable<any>{
+    return this._http.get(`${Enviroment.baseUrl}/notes`,{
+      headers:{
+        token: '3b8ny__' + window.localStorage.getItem('userToken')
+      }
+    })
+  }
+  deleteNote(id:string):Observable<any>{
+    return this._http.delete(`${Enviroment.baseUrl}/notes/${id}`,{
+      headers: {
+        token: '3b8ny__' + window.localStorage.getItem('userToken')
+      }
+    })
+  }
+}
