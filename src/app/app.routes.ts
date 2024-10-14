5ff4702988860663131a19e65824300c36e80100
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-10-14 21:03:00 +0300

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { logoutGuard } from './shared/guards/logout.guard';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home', pathMatch:'full'},
-    {path:'home',component:HomeComponent, canActivate:[authGuard],title:'home'},
-    {path:'login',component:LoginComponent, canActivate:[logoutGuard], title:'login'},
-    {path:'register',component:RegisterComponent,canActivate:[logoutGuard], title:'register'},
-    {path:'**', component:NotfoundComponent}
+    {path:'home',loadComponent:()=>import('../app/components/home/home.component').then((c)=>c.HomeComponent), canActivate:[authGuard],title:'home'},
+    {path:'login',loadComponent:()=>import('../app/auth/login/login.component').then((c)=>c.LoginComponent), canActivate:[logoutGuard], title:'login'},
+    {path:'register',loadComponent:()=>import('../app/auth/register/register.component').then((c)=>c.RegisterComponent),canActivate:[logoutGuard], title:'register'},
+    {path:'**',loadComponent:()=>import('../app/components/notfound/notfound.component').then((c)=>c.NotfoundComponent),title: 'notfound'}
 ];
